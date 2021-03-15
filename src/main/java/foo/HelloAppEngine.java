d6ffa8a2a3c1b4b85e3c376eddeb94a8bfcef997
mlopesstage <mica-lopes@dhotmail.fr>
2021-03-15 18:33:35 +0100

    Finalisation rendu intermédiaire

--- src/main/java/foo/HelloAppEngine.java
+++ src/main/java/foo/HelloAppEngine.java
@@ -7,19 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-    name = "HelloAppEngine",
+	name = "HelloAppEngine",
     urlPatterns = {"/petition"}
 )
 public class HelloAppEngine extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-
-    response.setContentType("text/plain");
-    response.setCharacterEncoding("UTF-8");
-
-    response.getWriter().print("Hello App Engine!\r\n");
-
-  }
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	  	response.setContentType("text/plain");
+	  	response.setCharacterEncoding("UTF-8");
+	  	response.getWriter().print("Hello App Engine!\r\n");
+  	}
 }
--- src/main/java/foo/PetitionEndpoint.java
+++ src/main/java/foo/PetitionEndpoint.java
@@ -135,6 +135,4 @@ public class PetitionEndpoint {
         }
 		return p;
 	}
-       
-	
 }
