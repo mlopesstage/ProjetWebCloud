8c62b41d148e0e190d9d011a3541602925c9dd0b
Luca <garic.luca@outlook.fr>
2021-04-01 17:22:00 +0200

    commit avant merge

--- src/main/java/foo/CreerPetition1.java
+++ src/main/java/foo/CreerPetition1.java
@@ -1,9 +1,9 @@
 package foo;
 
 public class CreerPetition1 {
-	public String owner; //voir si on supp ou pas
-	public String petName;
-	public String petProbleme;
+    public String owner; //voir si on supp ou pas
+    public String petName;
+    public String petProbleme;
 
-	public CreerPetition1() {}
+    public CreerPetition1() {}
 }
